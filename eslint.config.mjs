af9cacf9c4734f16536bca4a4ab38dc48bf988d1
iqlo <iqlohq@outlook.com>
2025-08-28 18:13:36 -0400

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,9 @@ export default tseslint.config(
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off'
     }
   },
   eslintConfigPrettier
